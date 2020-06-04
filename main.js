f1fdedade60dcb9b6ec8b86c77fe026cb4e375a5
IanKim <59094101+IanKim@users.noreply.github.com>
2020-06-04 14:16:53 -0400

    css

--- main.js
+++ main.js
@@ -19,3 +19,38 @@ const navbarMenu= document.querySelector('.navbar__menu');
 navbarToggleBtn.addEventListener('click',()=>{
     navbarMenu.classList.toggle('open');
 });
+
+// c72. remove selection from the previous item and select the next selection 
+
+const active = document.querySelector('.category__btn.selected');
+if(active !=null){
+  active.classList.remove('selected');
+
+}
+// e.target.classList.add('selected');
+
+// c70. project animation. 
+const projectContainer = document.querySelector('.work__projects');
+projectContainer.classList.add('anim-out');
+
+setTimeout(()=>{
+  projectContainer.classList.remove('anim-out');
+},300);
+
+// c68. forEach
+// const filter = e.target.dataset.filter
+
+// const projects = document.querySelectorAll('.project');
+// projects.forEach((project)=>{
+//   console.log(project.dataset.type);
+//   if(filter==='*'||)filter ===project.dataset.type{
+// project.classList.remove('invisible');
+//   }else{
+//     project.classList.add('invisible');
+
+//   }
+  
+
+
+
+// });
