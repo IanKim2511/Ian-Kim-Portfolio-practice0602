c02058e436f6fe019e6095c6765e8793d6c7f0e8
IanKim <59094101+IanKim@users.noreply.github.com>
2020-06-05 18:17:09 -0400

    js:navbar,contact,home

--- main.js
+++ main.js
@@ -12,73 +12,71 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// handle scrolling when tapping on the navbar menu.about__majors
+// const navbarMenu = document.querySelector(".navbar__menu");
+// navbarMenu.addEventListener("click", () => {
+//   console.log(event.target);
 
-//c74 navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-const navbarMenu= document.querySelector('.navbar__menu');
-navbarToggleBtn.addEventListener('click',()=>{
-    navbarMenu.classList.toggle('open');
-});
-
-
-// arrow-up. button when scrolling down
-// 1/2 of homeheight. adding visible class 
-
-const arrowUp= document.querySelector('.arrow-up');
-document.addEventListener('scroll'   ,()=>{
-  if(window.scrollY > homeHeight/2){
-    arrowUp.classList.add('visible');
-
-  }else{
-    arrowUp.classList.remove('visible');
-
-  }
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// c72. remove selection from the previous item and select the next selection 
-
-const active = document.querySelector('.category__btn.selected');
-if(active !=null){
-  active.classList.remove('selected');
-
-}
-// e.target.classList.add('selected');
-
-// c70. project animation. 
-const projectContainer = document.querySelector('.work__projects');
-projectContainer.classList.add('anim-out');
-
-setTimeout(()=>{
-  projectContainer.classList.remove('anim-out');
-},300);
-
-// c68. forEach
-// const filter = e.target.dataset.filter
-
-// const projects = document.querySelectorAll('.project');
-// projects.forEach((project)=>{
-//   console.log(project.dataset.type);
-//   if(filter==='*'||)filter ===project.dataset.type{
-// project.classList.remove('invisible');
-//   }else{
-//     project.classList.add('invisible');
-
+//   // ???
+//   const target = event.target;
+//   const link = target.dataset.link;
+//   if (link == null) {
+//     return;
 //   }
-  
+//   console.log(event.target.dataset.link);
+//   navbarMenu.classList.remove("open");
 
-
-
+//   const scrollto = document.querySelector(link);
+//   scrollTo.scrollIntoView({ behavior: "smooth" });
 // });
+
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (event) => {
+  console.log(event.target);
+
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+
+  // (74)
+  navbarMenu.classList.remove("open");
+
+  // mdn scrollIntoView
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
+
+// c74 .Navbar toggle button for small screen
+// show nav with clicking toggle button for small screen. 
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+navbarMenu.classList.toggle('open');
+});
+
+//Handle click on 'contact me' button on home
+// click menubutton. move to e.target
+
+const homeContactBtn=document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click',()=>{
+scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+  const scrollTo= document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+
+// make home slowly fade as window scrolls down
+//  opacity (window.scrollY / homnHeight)
+
+const home = document.querySelector('.home__container');
+const homeHeight=home.getBoundingClientRect().height;
+
+document.addEventListener('scroll',() =>{
+home.style.opacity=1-window.scrollY/homeHeight;
+});
